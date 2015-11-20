2e924d485a6cfca9058effe455b6debeb1b27b10
Kazuki Hasegawa <hasegawa@khasegawa.net>
2015-11-21 02:58:05 +0900

    add dotenv, handle error and refactor

---
+++ README.md
@@ -0,0 +1,12 @@
+= courier
+
+## Introduction
+TODO:
+
+## How to Setup
+1. `$ cp .env{.example,}`
+2. Rewite .env
+3. Run courier.
+
+## LICENSE
+This project rocks and uses MIT-LICENSE.
--- courier.go
+++ courier.go
@@ -10,9 +10,9 @@ import(
 	"regexp"
 
 	"github.com/fsouza/go-dockerclient"
+	"github.com/joho/godotenv"
 )
 
-// These headers won't be copied from original request to proxy request.
 var ignoredHeaderNames = []string{
 	"Connection",
 	"Keep-Alive",
@@ -24,90 +24,64 @@ var ignoredHeaderNames = []string{
 	"Upgrade",
 }
 
-// Provide host-based proxy server.
 type Proxy struct {
-	RequestConverter func(originalRequest, proxyRequest *http.Request)
+	RequestConverter func(r, proxyRequest *http.Request)
 	Transport http.RoundTripper
 }
 
-// Create a host-based reverse-proxy.
 func NewProxyWithHostConverter(hostConverter func(string) string) *Proxy {
 	return &Proxy{
-		RequestConverter: func(originalRequest, proxyRequest *http.Request) {
-			proxyRequest.URL.Host = hostConverter(originalRequest.Host)
+		RequestConverter: func(r, proxyRequest *http.Request) {
+			proxyRequest.URL.Host = hostConverter(r.Host)
 		},
 		Transport: http.DefaultTransport,
 	}
 }
 
-// Create a request-based reverse-proxy.
-func NewProxyWithRequestConverter(requestConverter func(*http.Request, *http.Request)) *Proxy {
-	return &Proxy{
-		RequestConverter: requestConverter,
-		Transport: http.DefaultTransport,
-	}
-}
+func (proxy *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	proxyRequest := proxy.copyRequest(r)
+	proxy.RequestConverter(r, proxyRequest)
 
-func (proxy *Proxy) ServeHTTP(writer http.ResponseWriter, originalRequest *http.Request) {
-	// Create a new proxy request object by coping the original request.
-	proxyRequest := proxy.copyRequest(originalRequest)
-
-	// Convert an original request into another proxy request.
-	proxy.RequestConverter(originalRequest, proxyRequest)
-
-	// Convert a request into a response by using its Transport.
-	response, err := proxy.Transport.RoundTrip(proxyRequest)
+	res, err := proxy.Transport.RoundTrip(proxyRequest)
 	if err != nil {
-		log.Printf("ErrorFromProxy: %v", err)
-		writer.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Unknown host %s", r.Host)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// Ensure a response body from upstream will be always closed.
-	defer response.Body.Close()
-
-	// Copy all header fields.
-	for key, values := range response.Header {
+	defer res.Body.Close()
+	for key, values := range res.Header {
 		for _, value := range values {
-			writer.Header().Add(key, value)
+			w.Header().Add(key, value)
 		}
 	}
 
-	// Copy a status code.
-	writer.WriteHeader(response.StatusCode)
-
-	// Copy a response body.
-	io.Copy(writer, response.Body)
+	w.WriteHeader(res.StatusCode)
+	io.Copy(w, res.Body)
 }
 
-// Create a new proxy request with some modifications from an original request.
-func (proxy *Proxy) copyRequest(originalRequest *http.Request) *http.Request {
+func (proxy *Proxy) copyRequest(r *http.Request) *http.Request {
 	proxyRequest := new(http.Request)
-	*proxyRequest = *originalRequest
+	*proxyRequest = *r
 	proxyRequest.Proto = "HTTP/1.1"
 	proxyRequest.ProtoMajor = 1
 	proxyRequest.ProtoMinor = 1
 	proxyRequest.Close = false
 	proxyRequest.Header = make(http.Header)
 	proxyRequest.URL.Scheme = "http"
-	proxyRequest.URL.Path = originalRequest.URL.Path
+	proxyRequest.URL.Path = r.URL.Path
 
-	// Copy all header fields.
-	for key, values := range originalRequest.Header {
+	for key, values := range r.Header {
 		for _, value := range values {
 			proxyRequest.Header.Add(key, value)
 		}
 	}
-
-	// Remove ignored header fields.
 	for _, headerName := range ignoredHeaderNames {
 		proxyRequest.Header.Del(headerName)
 	}
-
-	// Append this machine's host name into X-Forwarded-For.
-	if requestHost, _, err := net.SplitHostPort(originalRequest.RemoteAddr); err == nil {
-		if originalValues, ok := proxyRequest.Header["X-Forwarded-For"]; ok {
-			requestHost = strings.Join(originalValues, ", ") + ", " + requestHost
+	if requestHost, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		if values, ok := proxyRequest.Header["X-Forwarded-For"]; ok {
+			requestHost = strings.Join(values, ", ") + ", " + requestHost
 		}
 		proxyRequest.Header.Set("X-Forwarded-For", requestHost)
 	}
@@ -116,10 +90,13 @@ func (proxy *Proxy) copyRequest(originalRequest *http.Request) *http.Request {
 }
 
 func main() {
+	if godotenv.Load() != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	client, err := docker.NewClientFromEnv()
 	if err != nil {
-		log.Printf("Err: %v", err)
-		os.Exit(1)
+		log.Fatal("Err: %v", err)
 	}
 	dockerHost := os.Getenv("COURIER_DOCKER_HOST")
 	if dockerHost == "" {
@@ -127,16 +104,12 @@ func main() {
 	}
 	re := regexp.MustCompile(`^([a-zA-Z0-9]+)\.([0-9]+)\.`)
 
-	hostConverter := func(originalHost string) string {
+	converter := func(originalHost string) string {
 		group := re.FindSubmatch([]byte(originalHost))
-		if len(group) != 3 {
-			// TODO: Err
-		}
 		name := string(group[1])
 		container, err := client.InspectContainer(name)
 		if err != nil {
-			log.Printf("Err: %v", err)
-			os.Exit(1)
+			log.Printf("Container not found. %s", name)
 		}
 		log.Printf("Host: %v", originalHost)
 		_port := docker.Port(group[2]) + "/tcp"
@@ -144,11 +117,13 @@ func main() {
 		return dockerHost + ":" + containerPort
 	}
 
-	proxy := NewProxyWithHostConverter(hostConverter)
+	proxy := NewProxyWithHostConverter(converter)
 
 	port := os.Getenv("COURIER_PORT")
 	if port == "" {
-		port = "80"
+		port = "8080"
 	}
+
+	log.Printf("Listen %s", port)
 	http.ListenAndServe(":" + port, proxy)
 }
